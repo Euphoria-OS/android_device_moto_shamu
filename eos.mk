aadeac681890fd57f9334bc0d0ef79f878978d28
David Smit <dsmitty166@gmail.com>
2015-03-25 19:10:32 -0500

    Update fingerprint to LMY47M

--- eos.mk
+++ eos.mk
@@ -15,6 +15,8 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
 
+BUILD_FINGERPRINT=google/shamu/shamu:5.1/LMY47M/1774234:user/release-keys \
+PRIVATE_BUILD_DESC="shamu-user 5.1 LMY47M 1774234 release-keys"
 # Enable Torch
 #PRODUCT_PACKAGES += Torch
 
