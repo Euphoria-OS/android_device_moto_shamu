4332f4fb7cb5c8fae93509bb5c9a04837a1a2644
David Smit <dsmitty166@gmail.com>
2015-04-18 14:58:36 -0500

    kernel:Build hells kernel inline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 TARGET_NO_BOOTLOADER := true
 
 # Inline kernel building		
-TARGET_KERNEL_CONFIG := lk_defconfig		
+TARGET_KERNEL_CONFIG := hells_defconfig		
 TARGET_KERNEL_SOURCE := kernel/moto/shamu		
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb		
 
