1bcc788f70c1c79c36e8790a224ab380c9bfc2f7
David Smit <dsmitty166@gmail.com>
2015-05-30 22:35:52 -0500

    Update fingerprint to 5.1.1
    Change-Id: I5dddb2e4d325deefdc6f69de809a045da74a04f2

--- eos.mk
+++ eos.mk
@@ -16,8 +16,8 @@ PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-BUILD_FINGERPRINT=google/shamu/shamu:5.1/LMY47I/1767468:user/release-keys \
-PRIVATE_BUILD_DESC="shamu-user 5.1 LMY47I 1767468 release-keys" 
+BUILD_FINGERPRINT=google/shamu/shamu:5.1.1/LYZ28E/1914015:user/release-keys \
+PRIVATE_BUILD_DESC="shamu-user 5.1.1 LYZ28E 1914015 release-keys" 
 
 # Enable Torch
 #PRODUCT_PACKAGES += Torch
